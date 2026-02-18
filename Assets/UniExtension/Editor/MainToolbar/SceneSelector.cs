ce6c132198239a55e6f6d255346b7dca22a48296
TaibkKurbanaliev <taibkurbanaliev@gmail.com>
2026-02-18 11:15:53 +0400

    feat: Add support for unity version < 6.3

--- Assets/UniExtension/Editor/MainToolbar/SceneSelector.cs
+++ Assets/UniExtension/Editor/MainToolbar/SceneSelector.cs
@@ -1,42 +1,52 @@
-using System;
 using System.IO;
+using System.Reflection;
 using UnityEditor;
 using UnityEditor.SceneManagement;
+#if UNITY_6000_3_OR_NEWER
 using UnityEditor.Toolbars;
+#endif
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
 namespace UniExtension.Editor
 {
+    [InitializeOnLoad]
     public class SceneSelector
     {
         private const string k_ElementPath = "Scene/Selector";
-        private static string[] s_ScenePaths;
+        private static string[] _scenePaths;
 
-        [MainToolbarElement("Scene/Selector", defaultDockPosition = MainToolbarDockPosition.Middle)]
+#if UNITY_6000_3_OR_NEWER
+        [MainToolbarElement(k_ElementPath, defaultDockPosition = MainToolbarDockPosition.Middle)]
         public static MainToolbarElement Selector()
         {
-            string activeSceneName;
-            if (Application.isPlaying)
-                activeSceneName = SceneManager.GetActiveScene().name;
-            else
-                activeSceneName = EditorSceneManager.GetActiveScene().name;
-            if (activeSceneName.Length == 0)
-                activeSceneName = "Untitled";
-
-            var icon = EditorGUIUtility.IconContent("UnityLogo").image as Texture2D;
-            var content = new MainToolbarContent(activeSceneName, icon, "Select active scene");
+            GetToolbarData(out var text, out var icon);
+            var content = new MainToolbarContent(text, icon, "Select active scene");
             return new MainToolbarDropdown(content, ShowDropdownMenu);
+        }
+#endif
+
+        static void GetToolbarData(out string sceneName, out Texture2D icon)
+        {
+            sceneName = Application.isPlaying
+                ? SceneManager.GetActiveScene().name
+                : EditorSceneManager.GetActiveScene().name;
+
+            if (string.IsNullOrEmpty(sceneName))
+                sceneName = "Untitled";
+
+            icon = EditorGUIUtility.IconContent("UnityLogo").image as Texture2D;
         }
 
         static void ShowDropdownMenu(Rect dropDownRect)
         {
             var menu = new GenericMenu();
-            if (s_ScenePaths.Length == 0)
+            if (_scenePaths.Length == 0)
             {
                 menu.AddDisabledItem(new GUIContent("No Scenes in Project"));
             }
-            foreach (string scenePath in s_ScenePaths)
+            foreach (string scenePath in _scenePaths)
             {
                 string sceneName = Path.GetFileNameWithoutExtension(scenePath);
                 menu.AddItem(new GUIContent(sceneName), false, () =>
@@ -81,14 +91,16 @@ namespace UniExtension.Editor
 
         static void RefreshSceneList()
         {
-            s_ScenePaths = Directory.GetFiles("Assets", "*.unity", SearchOption.AllDirectories);
+            _scenePaths = Directory.GetFiles("Assets", "*.unity", SearchOption.AllDirectories);
         }
 
         static void SceneSwitched(Scene oldScene, Scene newScene)
         {
+#if UNITY_6000_3_OR_NEWER
             MainToolbar.Refresh(k_ElementPath);
+#endif
         }
-
+#if UNITY_6000_3_OR_NEWER
         static SceneSelector()
         {
             RefreshSceneList();
@@ -96,5 +108,69 @@ namespace UniExtension.Editor
             SceneManager.activeSceneChanged += SceneSwitched;
             EditorSceneManager.activeSceneChangedInEditMode += SceneSwitched;
         }
+#endif
+
+#if !UNITY_6000_3_OR_NEWER
+        static IMGUIContainer _legacyContainer;
+
+        static SceneSelector()
+        {
+            RefreshSceneList();
+
+            EditorApplication.projectChanged += RefreshSceneList;
+            SceneManager.activeSceneChanged += SceneSwitched;
+            EditorSceneManager.activeSceneChangedInEditMode += SceneSwitched;
+
+            EditorApplication.delayCall += TryAttachToLegacyToolbar;
+        }
+
+        static void TryAttachToLegacyToolbar()
+        {
+            var toolbarType = typeof(UnityEditor.Editor).Assembly.GetType("UnityEditor.Toolbar");
+            var toolbars = Resources.FindObjectsOfTypeAll(toolbarType);
+
+            if (toolbars.Length == 0)
+            {
+                EditorApplication.delayCall += TryAttachToLegacyToolbar;
+                return;
+            }
+
+            var toolbar = toolbars[0];
+            var root = toolbarType
+                .GetField("m_Root", BindingFlags.NonPublic | BindingFlags.Instance)
+                ?.GetValue(toolbar) as VisualElement;
+
+            if (root == null)
+                return;
+
+            // Центральная зона (кнопки Play)
+            var centerZone = root.Q("ToolbarZonePlayMode");
+            if (centerZone == null)
+                return;
+
+            _legacyContainer = new IMGUIContainer(DrawLegacyToolbarGUI)
+            {
+                style =
+                {
+                    marginLeft = 6,
+                    marginRight = 6
+                }
+            };
+
+            centerZone.Add(_legacyContainer);
+        }
+
+        static void DrawLegacyToolbarGUI()
+        {
+            GetToolbarData(out var text, out _);
+
+            if (GUILayout.Button(text, EditorStyles.popup, GUILayout.Width(160)))
+            {
+                var rect = GUILayoutUtility.GetLastRect();
+                rect.y += rect.height;
+                ShowDropdownMenu(rect);
+            }
+        }
+#endif
     }
 }
--- Assets/UniExtension/Editor/MainToolbar/TimescaleSlider.cs
+++ Assets/UniExtension/Editor/MainToolbar/TimescaleSlider.cs
@@ -1,20 +1,25 @@
+﻿using System.Reflection;
 using UnityEditor;
+#if UNITY_6000_3_OR_NEWER
 using UnityEditor.Toolbars;
+#endif
 using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace UniExtension.Editor
 {
+    [InitializeOnLoad]
     public class TimescaleSlider
     {
         const float k_MinTimescale = 0f;
         const float k_MaxTimescale = 5f;
         const float k_Padding = 10f;
-
+#if UNITY_6000_3_OR_NEWER
         [MainToolbarElement("Timescale/Slider", defaultDockPosition = MainToolbarDockPosition.Middle)]
         public static MainToolbarElement TimeSlider()
         {
             var content = new MainToolbarContent("Timescale", "Timescale");
-            var slider = new MainToolbarSlider(content, Time.timeScale,k_MinTimescale, 
+            var slider = new MainToolbarSlider(content, Time.timeScale, k_MinTimescale,
                                                k_MaxTimescale, OnSliderValueChanged);
 
             return slider;
@@ -26,8 +31,8 @@ namespace UniExtension.Editor
             var icon = EditorGUIUtility.IconContent("Refresh").image as Texture2D;
             var content = new MainToolbarContent(icon, "Reset");
             var button = new MainToolbarButton(content, () => {
-                    Time.timeScale = 1f;
-                    MainToolbar.Refresh("Timescale/Slider");
+                Time.timeScale = 1f;
+                MainToolbar.Refresh("Timescale/Slider");
             });
 
             return button;
@@ -37,5 +42,76 @@ namespace UniExtension.Editor
         {
             Time.timeScale = value;
         }
+#endif
+#if !UNITY_6000_3_OR_NEWER
+        static IMGUIContainer _sliderContainer;
+
+        static TimescaleSlider()
+        {
+            EditorApplication.delayCall += TryAttachToLegacyToolbar;
+        }
+
+        static void TryAttachToLegacyToolbar()
+        {
+            var toolbarType = typeof(UnityEditor.Editor)
+                .Assembly
+                .GetType("UnityEditor.Toolbar");
+
+            var toolbars = Resources.FindObjectsOfTypeAll(toolbarType);
+            if (toolbars.Length == 0)
+            {
+                EditorApplication.delayCall += TryAttachToLegacyToolbar;
+                return;
+            }
+
+            var toolbar = toolbars[0];
+            var root = toolbarType
+                .GetField("m_Root", BindingFlags.NonPublic | BindingFlags.Instance)
+                ?.GetValue(toolbar) as VisualElement;
+
+            if (root == null)
+                return;
+
+            var centerZone = root.Q("ToolbarZonePlayMode");
+            if (centerZone == null)
+                return;
+
+            _sliderContainer = new IMGUIContainer(DrawLegacyGUI)
+            {
+                style =
+                {
+                    marginLeft = 6,
+                    marginRight = 6
+                }
+            };
+
+            centerZone.Add(_sliderContainer);
+        }
+
+        static void DrawLegacyGUI()
+        {
+            GUILayout.BeginHorizontal(GUILayout.Width(150));
+
+            EditorGUI.BeginChangeCheck();
+            float value = GUILayout.HorizontalSlider(
+                Time.timeScale,
+                k_MinTimescale,
+                k_MaxTimescale,
+                GUILayout.Width(120)
+            );
+
+            if (EditorGUI.EndChangeCheck())
+                Time.timeScale = value;
+
+            GUILayout.Label(Time.timeScale.ToString("0.00"),
+                            EditorStyles.miniLabel,
+                            GUILayout.Width(32));
+
+            if (GUILayout.Button("↩", GUILayout.Width(24)))
+                Time.timeScale = 1f;
+
+            GUILayout.EndHorizontal();
+        }
+#endif
     }
 }
